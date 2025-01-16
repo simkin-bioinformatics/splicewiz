d16cd12efd27a5b8ea9e04468b7fc6aaffcf4f16
Charlie Simkin <97923672+charliesimkin@users.noreply.github.com>
2025-01-16 11:52:22 -0500

    Update 02_alignment.R

--- 02_alignment.R
+++ 02_alignment.R
@@ -21,7 +21,7 @@ fastq_files <- findFASTQ(
     sample_path = config$fastq_folder_path, 
     paired = config$is_paired,
     fastq_suffix = config$fastq_suffix,
-    level = 0
+    level = 1
 )
 
 # perform alignent
